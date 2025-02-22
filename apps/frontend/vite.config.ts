91f2a30bd51845786db4a79f8c2db374bd2308de
Thomas Carmichael <ThomasCarmichael@pm.me>
2025-02-21 20:59:54 -0600

    chore: unplug tailwind for now, use module.css

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import solid from 'vite-plugin-solid';
-import tailwind from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [solid(), tailwind()],
+  plugins: [solid()],
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
