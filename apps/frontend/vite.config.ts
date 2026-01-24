fb1e4b74d715ed07c17d946bd536d0fda7e3433b
Thomas Carmichael <ThomasCarmichael@pm.me>
2026-01-23 21:45:01 -0600

    fix: mobile touch support, and vite conf

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     solid(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'assets/**/*', 'stockfish/**/*'],
+      includeAssets: ['favicon.svg', 'stockfish/**/*'],
       devOptions: {
         enabled: true,
         suppressWarnings: true,
